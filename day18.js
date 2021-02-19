a77a40b9d01a3ebb62a48870c46e2777ab1c160e
Hugo Mosh <hugomosh@gmail.com>
2021-02-19 08:40:16 -0800

    Day 18 part 1 done. Now to do postfix for part2

--- day18.js
+++ day18.js
@@ -1,15 +1,75 @@
 var utils = require("./utils.js");
 const input = utils.readLinesFromFile("./input/day18.txt");
 
+function extractBigParenthesis(input) {
+  const q = [...input];
+  const subQ = [];
+  // an operation without the first parenthesis
+  let openParenthesisCount = 1;
+  let c = q.shift();
+  let started = false;
+  while (openParenthesisCount) {
+    c = q.shift();
+    if (c === "(") {
+      openParenthesisCount++;
+    } else if (c === ")") {
+      openParenthesisCount--;
+      if (openParenthesisCount == 0) {
+        continue;
+      }
+    }
+    subQ.push(c);
+  }
+  console.assert(subQ.length, subQ);
+  //console.log({ subQ, q });
+  return [subQ, q];
+}
+
+function solveEquation(input = []) {
+  //console.log({input});
+  let q = [...input];
+  let leftV, operator, rightV;
+  let c;
+  while (q.length) {
+    //console.log(q);
+    c = q.shift();
+    if (c === "+" || c === "*") {
+      operator = c;
+      continue;
+    }
+    if (c === "(") {
+      q.unshift(c);
+      const [subQ, newQ] = extractBigParenthesis(q);
+      q = newQ;
+      c = solveEquation(subQ);
+    } else if (c === ")") {
+      continue;
+    }
+    if (!leftV) {
+      leftV = Number(c);
+    } else {
+      rightV = Number(c);
+      const newV = operator === "+" ? leftV + rightV : leftV * rightV;
+      //console.log(leftV, operator, rightV, "=", newV);
+      q.unshift(newV);
+      leftV = undefined;
+      operator = undefined;
+      rightV = undefined;
+    }
+  }
+  // console.log(leftV);
+  console.assert(Number(c), leftV);
+  return c;
+}
+
 function day18P1(input) {
-  let validPassCount = 0;
+  const regex = new RegExp("([0-9]+)|([+()*]{1})", "g");
   const res = input.map((line) => {
-    const inputParts = eval(line);
-    validPassCount += inputParts;
-    return inputParts;
+    const inputParts = line.match(regex);
+    return solveEquation(inputParts);
   });
-  console.log(res);
-  return validPassCount;
+
+  return res.reduce((a, b) => a + b, 0);
 }
 
 function day18P2(input) {
@@ -30,29 +90,45 @@ sol = day18(input);
 
 console.log({ sol });
 
-const test = `2 * 3 + (4 * 5)
-5 + (8 * 3 + 9 + 3 * 4 * 3)
-5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
-((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`;
+const tA1 = `1 + 2 * 3 + 4 * 5 + 6
+1 + (2 * 3) + (4 * (5 + 6))`;
+const t1 = `1 + 2 * 3 + 4 * 5 + 6`;
+const t2 = `1 + (2 * 3) + (4 * (5 + 6))`;
 
-function testDemo() {
-  const actual = day18P1(utils.splitLines(test));
-  const expected = [26, 437, 12240, 13632].reduce((acc, cur) => acc + cur);
+const t3 = `(8 + 4 + 9 + (7 * 9 + 4 + 6 + 9) * 3) + 6 + 5 + ((8 * 2) + 2 * 9) * 5`;
+const t4 = `3 + (4 + 8 + 3 + 4 + 7 + 6) + 4 + 3 * 4 + ((5 * 6) + 2 * 5 * 2 + 8 * 3)`;
+///e :6024
+const t5 = `((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`;
 
-  console.assert(
-    actual === expected,
-    `Expected: ${expected} actual ${actual}.`
-  );
+const testCases = [
+  [t1, 71, 231],
+  [t2, 51, 51],
+  [tA1, 71 + 51, 231 + 51],
+  [t3, 2410, 1],
+  [t4, 1152, 1],
+  [t5, 13632, 23340],
+];
+function testDemoP1() {
+  for (let i = 0; i < testCases.length; i++) {
+    const [aInput, expected] = testCases[i];
+    const actual = day18P1(utils.splitLines(aInput));
+    console.assert(
+      actual === expected,
+      `Expected: ${expected} actual ${actual}.`
+    );
+  }
 }
 
-function testDemo2() {
-  const actual = day18P2(utils.splitLines(test));
-  const expected = 123123123;
-  console.assert(
-    actual === expected,
-    `Expected: ${expected} actual ${actual}.`
-  );
+function testDemoP2() {
+  for (let i = 0; i < testCases.length; i++) {
+    const [aInput, , expected] = testCases[i];
+    const actual = day18P2(utils.splitLines(aInput));
+    console.assert(
+      actual === expected,
+      `Expected: ${expected} actual ${actual}.`
+    );
+  }
 }
 
-testDemo();
-//testDemo2();
+testDemoP1();
+testDemoP2();
